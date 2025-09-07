b90ad1bd42eee84378f5cea5feae628adbd6a586
Mostafa <m.mohammadimehr1390@gmail.com>
2025-09-07 13:28:12 +0330

    Async & Await #3

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,28 @@
-const myfunction =async () => "hello world";
-console.log(myfunction());
+const hasMeeting = false;
+const meeting = new Promise((resolve, reject) => {
+  if (!hasMeeting) {
+    const meetingdetails = {
+      name: "Codeyad Meeting",
+      location: "Tehran",
+      time: "1:00 PM",
+    };
+    resolve(meetingdetails);
+  } else {
+    reject(new Error("Meeting Canceled..."));
+  }
+});
+const addToCalendar = (meetingDetails) => {
+  const calendar = `${meetingDetails.name} is scheduled at ${meetingDetails.time} on
+     ${meetingDetails.location}`;
+  return Promise.resolve(calendar);
+};
+// meeting
+//   .then(addToCalendar)
+//   .then((res) => console.log(res))
+//   .catch((rej) => console.log(rej));
+const mymeting = async () => {
+  const meetingDetails = await meeting;
+  const message = await addToCalendar(meetingDetails);
+  console.log(message);
+};
+mymeting();
